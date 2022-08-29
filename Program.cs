28560540b0a6f93c4e434d05489d60c98ac607cc
Dmirty <9048881951@mail.ru>
2022-08-29 22:15:42 +0500

    #3

--- Program.cs
+++ Program.cs
@@ -64,6 +64,54 @@ PrintArray(matrix);
 // Программа считает сумму элементов в каждой строке и
 //  выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+/*
+int[,] GetArray(int m, int n) //Метод ввод массива
+{
+	int[,] matrix = new int[m, n];
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{ matrix[i, j] = new Random().Next(11); }
+	}
+	return matrix;
+}
+void PrintArray(int[,] matrix) //Метод печать массива
+
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{ Console.Write(matrix[i, j] + "\t"); }
+		Console.WriteLine();
+	}
+}
+
+int sizeArray = 5;
+int[,] matrix = GetArray(sizeArray, sizeArray);
+int[] matrixSumm = new int[sizeArray];
+PrintArray(matrix); Console.WriteLine();
+
+
+int summ = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+	for (int j = 0; j < matrix.GetLength(1); j++)
+	{ summ += matrix[i, j]; }
+	matrixSumm[i] = summ;
+	summ = 0;
+}
+//for (int i = 0; i < matrixSumm.Length; i++) //Показать массив сумм
+//{ Console.Write(matrixSumm[i] + "\t"); }
+
+int temp = int.MaxValue;
+for (int i = 0; i < matrixSumm.Length; i++)
+{
+	if (matrixSumm[i] < temp)
+	{ temp = matrixSumm[i]; summ = i+1; }
+}
+
+Console.WriteLine($"Номер строки с наименьшей суммой чисел - {summ}");
+*/
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая
 //  будет находить произведение двух матриц.
@@ -73,6 +121,64 @@ PrintArray(matrix);
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] GetArray(int m, int n) //Метод ввод массива
+{
+	int[,] matrix = new int[m, n];
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{ matrix[i, j] = new Random().Next(11); }
+	}
+	return matrix;
+}
+void PrintArray(int[,] matrix) //Метод печать массива
+
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{ Console.Write(matrix[i, j] + "\t"); }
+		Console.WriteLine();
+	}
+}
+int sizeArray = 5;
+int[,] matrix1 = GetArray(sizeArray, sizeArray);
+int[,] matrix2 = GetArray(sizeArray, sizeArray);
+int[,] matrixResult = GetArray(sizeArray, sizeArray);
+
+for (int i = 0; i < matrixResult.GetLength(0); i++)
+{
+	for (int j = 0; j < matrixResult.GetLength(1); j++)
+	{ matrixResult[i, j] = matrix1[i, j] * matrix2[i, j]; }
+}
+Console.WriteLine("Массив 1");
+PrintArray(matrix1); Console.WriteLine();
+
+Console.WriteLine("Массив 2");
+PrintArray(matrix2); Console.WriteLine();
+
+Console.WriteLine("Результирующий массив");
+PrintArray(matrixResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся 
